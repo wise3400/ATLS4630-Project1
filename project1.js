4551aa4d9dbf0d83fe25bfd42b0fe5bae07b23cf
Wilson Seet <70073191+wise3400@users.noreply.github.com>
2023-10-24 17:46:13 -0600

    10/24

--- project1.js
+++ project1.js
@@ -23,25 +23,27 @@ function showWeatherInfo(getWeatherData)
     document.getElementById("humid").innerHTML = "Humidity: " + "<br>" + getWeatherData.main.humidity + "%";
     document.getElementById("feels-like").innerHTML = "Feels like: " + "<br>" + getWeatherData.main.feels_like + "°C";
     
-   
+    //const showImg = document.getElementById("pics").style.display = 'block';
     // https://media.tenor.com/gznLWsJjaMAAAAAC/daytime-miving.gif
     if(getWeatherData.weather[0].description == "clear sky")
     {
        document.getElementById("pics").src = "clear-sky.gif";
        document.getElementById("pics").alt = "Clear Sky";
-    
+       document.getElementById("pics").style.display = 'block';
     }
     // https://media.tenor.com/D8N2aMZtd4YAAAAC/rain.gif
     if(getWeatherData.weather[0].description == "moderate rain")
     {
        document.getElementById("pics").src = "rain.gif";
        document.getElementById("pics").alt = "Raining";
+       document.getElementById("pics").style.display = 'block';
     }
     // https://media.tenor.com/WhD4AWN30YkAAAAC/clouds-moving.gif
     if(getWeatherData.weather[0].description == "scattered clouds")
     {
        document.getElementById("pics").src = "scattered-clouds.gif";
        document.getElementById("pics").alt = "Scattered Clouds";
+       document.getElementById("pics").style.display = 'block';
     }
 
    // https://i.gifer.com/7RtV.gif
@@ -49,6 +51,7 @@ function showWeatherInfo(getWeatherData)
    {
         document.getElementById("pics").src = "overcast.gif";
         document.getElementById("pics").alt = "Overcast Clouds";
+        document.getElementById("pics").style.display = 'block';
    }
 
    // https://media.tenor.com/5sMqxZU0224AAAAd/heavy-rain-rainy-day.gif
@@ -56,6 +59,7 @@ function showWeatherInfo(getWeatherData)
    {
          document.getElementById("pics").src = "heavy-rain.gif";
          document.getElementById("pics").alt = "Heavy Rain";
+         document.getElementById("pics").style.display = 'block';
    }
 
    // https://media.tenor.com/NsxcRYfc_PYAAAAC/street-lights-rain.gif
@@ -63,36 +67,42 @@ function showWeatherInfo(getWeatherData)
    {
          document.getElementById("pics").src = "light-rain.gif";
          document.getElementById("pics").alt = "Light Rain";
+         document.getElementById("pics").style.display = 'block';
    }
    // https://media.tenor.com/szl0uLjxqVYAAAAC/snow-light.gif
    if(getWeatherData.weather[0].description == "light snow")
    {
          document.getElementById("pics").src = "light-snow.gif";
          document.getElementById("pics").alt = "Light Snow";
+         document.getElementById("pics").style.display = 'block';
    }
    // https://media0.giphy.com/media/JjrDsvilNKgw0/giphy.gif
    if(getWeatherData.weather[0].description == "heavy intensity rain")
    {
          document.getElementById("pics").src = "heavy-rain1.gif";
          document.getElementById("pics").alt = "Heavy Rain";
+         document.getElementById("pics").style.display = 'block';
    }
    // https://i.pinimg.com/originals/d9/a6/ee/d9a6eeb44e2edf61d515c7622264c2fc.gif
    if(getWeatherData.weather[0].description == "few clouds")
    {
           document.getElementById("pics").src = "few-clouds.gif";
           document.getElementById("pics").alt = "Few Clouds";
+          document.getElementById("pics").style.display = 'block';
    }
    // https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/625a747a-061b-477d-958f-a0d6cea9e4cb/dax9bd4-dd0da73d-5b6e-415c-b05e-19471f366e5a.jpg/v1/fill/w_1024,h_768,q_75,strp/broken_clouds_by_kevintheman_dax9bd4-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzY4IiwicGF0aCI6IlwvZlwvNjI1YTc0N2EtMDYxYi00NzdkLTk1OGYtYTBkNmNlYTllNGNiXC9kYXg5YmQ0LWRkMGRhNzNkLTViNmUtNDE1Yy1iMDVlLTE5NDcxZjM2NmU1YS5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.2HBtScMyydNDUe606gk2Jd8RHs6iM-76feSI7Dc3sLw
    if(getWeatherData.weather[0].description == "broken clouds")
    {
          document.getElementById("pics").src = "broken-clouds.jpg";
          document.getElementById("pics").alt = "Broken Clouds";
+         document.getElementById("pics").style.display = 'block';
    }
    //https://i.gifer.com/7Jy7.gif
    if(getWeatherData.weather[0].description == "smoke")
    {
          document.getElementById("pics").src = "hazy.gif";
          document.getElementById("pics").alt = "Hazy";
+         document.getElementById("pics").style.display = 'block';
    }
 
 }
