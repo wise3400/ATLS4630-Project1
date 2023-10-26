b0a6a1b38a6e2a57be3c5045e2792ecd3310d6c0
Wilson Seet <70073191+wise3400@users.noreply.github.com>
2023-10-25 22:28:41 -0600

    10.25

--- project1.js
+++ project1.js
@@ -28,7 +28,7 @@ function showWeatherInfo(getWeatherData)
 {
     console.log(getWeatherData);
 
-    // Get rid of the not found error if the location is valid.
+    // Gets rid of the not found error if the location is valid.
     document.getElementById("notFound").style.display = 'none';
 
     document.getElementById("Place").innerHTML = "Location: " + "<br>" + getWeatherData.name + ", " + getWeatherData.sys.country;
@@ -54,7 +54,6 @@ function showWeatherInfo(getWeatherData)
     document.getElementById("feels-like").style.display = 'block';
    
     
-    //const showImg = document.getElementById("pics").style.display = 'block';
     // https://media.tenor.com/gznLWsJjaMAAAAAC/daytime-miving.gif
     if(getWeatherData.weather[0].description == "clear sky")
     {
@@ -149,36 +148,3 @@ function showWeatherInfo(getWeatherData)
          document.getElementById("pics").style.display = 'block';
    }
 }
-
-
-
-// function writeDogFactsToPage(json) {
-//   for (const dogFact of json.data) {
-//     const p = document.createElement('p');
-//     console.log(dogFact);
-//     p.innerText = dogFact.attributes.name + ", " +  dogFact.attributes.description;
-   
-//     document.body.appendChild(p);
-//   }
-// }
-
-// fetch('https://dogapi.dog/api/v2/breeds')
-//   .then(response => response.json())
-//   .then(writeDogFactsToPage);
-
-// // pexels API
-// const myQuery = 'Dogs';
-// fetch(`https://api.pexels.com/v1/search?query=${myQuery}&per_page=13`, {
-//   type: 'GET',
-//   headers: { Authorization: 'rUAeaXs8i1jb1agcojt7e1611gLOmko7P5rOtsdjLttQAXRI5WlJ4lZm' }
-// }).then(response => response.json())
-//   .then(json => {
-//     for (const photo of json.photos) {
-//       const img = document.createElement('img');
-//       img.src = photo.src.small;
-//       img.alt = photo.alt;
-//       document.body.appendChild(img);
-//     }
-//   });
-
-// const box = document.querySelector('.box');
